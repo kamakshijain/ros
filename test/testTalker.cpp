d18799b09ac437dedd396235457c7edb08765196
kamakshijain <kamakshi@terpmail.umd.edu>
2019-11-07 23:31:24 -0500

    Added 3 test cases and moved test launch file to test folder

---
+++ test/testTalker.cpp
@@ -0,0 +1,84 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Kamakshi Jain
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *  @file    testTalker.cpp
+ *  @author  Kamakshi Jain
+ *  @copyright MIT License
+ *
+ *  @brief  ENPM808X :Assignment to implement ROS publisher and subscriber nodes
+ *
+ *  @section DESCRIPTION
+ *
+ *  This program is a part of the beginner tutorials in ROS
+ *  It defines the publisher (talker)
+ *
+ */
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+#include <ros/service_client.h>
+#include <tf/transform_listener.h>
+#include "beginner_tutorials/change_string.h"
+
+/**
+ * @brief Test case to check the existence of the change_string service
+ * @param none
+ * @return none
+ */
+TEST(TESTSuite, testServiceExistence) {
+    ros::NodeHandle n;
+    ros::ServiceClient client =
+               n.serviceClient<beginner_tutorials::change_string>(
+                     "change_string");
+    bool exists(client.waitForExistence(ros::Duration(5)));
+    EXPECT_TRUE(exists);
+}
+
+/**
+ * @brief Test case to check the execution of the change_string service
+ * @param none
+ * @return none
+ */
+TEST(TESTSuite, testServiceRun) {
+    ros::NodeHandle n;
+    ros::ServiceClient client =
+               n.serviceClient<beginner_tutorials::change_string>(
+                         "change_string");
+    beginner_tutorials::change_string srv;
+    srv.request.input = "new_string";
+    client.call(srv);
+    EXPECT_STREQ("new_string", srv.response.output.c_str());
+}
+
+/**
+ * @brief Run all rostests for the talker node
+ *
+ * @param none
+ * @return 0 on success
+ */
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "testTalker");
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
